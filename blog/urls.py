0d56f82fdc5e91b748527276fc8011f3b019f3e6
rashdogg74 <pauloriordan74@gmail.com>
2022-06-17 17:58:27 +0000

    Add add post functionality

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomeView, PostDetailView
+from .views import HomeView, PostDetailView, AddPostView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='index'),
     path('post/<int:pk>', PostDetailView.as_view(), name='post_detail'),
+    path('add_post/', AddPostView.as_view(), name='add_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -15,3 +15,9 @@ class HomeView(generic.ListView):
 class PostDetailView(generic.DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+
+class AddPostView(generic.CreateView):
+    model = Post
+    template_name = 'add_post.html'
+    fields = '__all__'
