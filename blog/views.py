8a4bb3d431fffa8c9883e553db75b93e8ab042f3
rashdogg74 <pauloriordan74@gmail.com>
2022-10-03 21:16:24 +0000

    Add new footer and navbar... footer needs stylnig

--- blog/views.py
+++ blog/views.py
@@ -1,2 +1,13 @@
 from django.shortcuts import render, get_object_or_404
-from django.views import generic, View
+from django.views import generic
+from .models import Post
+
+
+class PostList(generic.ListView):
+    """
+    Post List Class Based View for displaying current article inventory
+    """
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6
