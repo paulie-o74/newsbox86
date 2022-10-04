bb633fdfd4b420b4eb57839e81352ef2e10fa725
rashdogg74 <pauloriordan74@gmail.com>
2022-10-04 20:12:07 +0000

    Add like functionality

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
+from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import CommentForm
+
 
 
 class PostList(generic.ListView):
