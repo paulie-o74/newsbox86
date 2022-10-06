80eff7ef953aec1692d41349860e7cdf68035618
rashdogg74 <pauloriordan74@gmail.com>
2022-10-06 19:33:38 +0000

    Add 404 page

--- newsbox86/urls.py
+++ newsbox86/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls'), name='blog_urls'),
     path('accounts/', include('allauth.urls'))
 ]
+
+handler404 = 'newsbox86.views.handler404'
