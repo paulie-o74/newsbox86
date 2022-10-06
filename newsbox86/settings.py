20dcf89d57c75e9778cb4f01fc206236d3c3bdd9
rashdogg74 <pauloriordan74@gmail.com>
2022-10-06 19:56:11 +0000

    Deployment commit

--- newsbox86/settings.py
+++ newsbox86/settings.py
@@ -32,7 +32,7 @@ DEBUG = True
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-ALLOWED_HOSTS = ['newsbox86.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['thenewsbox74.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -111,16 +111,16 @@ WSGI_APPLICATION = 'newsbox86.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES = {
+   'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
 
 
 # Password validation
